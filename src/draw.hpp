946346647ebfd771f328e96b86e9a16f6c1e11d3
Motisma479 <mat020930@gmail.com>
2022-04-15 01:03:38 +0200

    project move to mainbranch

--- src/draw.hpp
+++
@@ -1,6 +0,0 @@
-#include "common.hpp"
-
-// Update The Game
-void updateGame(Game &game);
-// Draw The Game
-void drawGame(Game &game);
--- src/loader.hpp
+++
@@ -1,5 +0,0 @@
-#include "common.hpp"
-
-//read Dialogue, Save
-void readAllFile(const char* dialogue, const char* save, Game &game);
-
